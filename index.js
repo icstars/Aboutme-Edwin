2f78b18e0a2442d497174eb52ac44c351c4f155c
cothao <114521616+cothao@users.noreply.github.com>
2023-06-29 09:09:47 -0500

    added new branch

--- index.js
+++ index.js
@@ -79,8 +79,8 @@
 
 // // //     alert("user doesn't exist")
 
-// // /* else if; will run if condition 
-// // before it are false and specifies a 
+// // /* else if; will run if condition
+// // before it are false and specifies a
 // // new condition*/
 
 // // // let q = 0
@@ -122,13 +122,9 @@
 // // //   alert("you are incorrect");
 // // // }
 
-
-
-
-
-// // ////////////Loops 
+// // ////////////Loops
 // // let myName= "Lucycan"
-// // console.log(myName); 
+// // console.log(myName);
 // // console.log(typeof myName);
 
 // // //// conditionals
@@ -139,9 +135,9 @@
 // // }
 // // /// for
 // // for(let i = 0; i< 10; i++) {
-//   //     console.log(i); 
+//   //     console.log(i);
 //   // }
-//   // //if you infinite loop your machine will crash// 
+//   // //if you infinite loop your machine will crash//
 
 //   //ANCHOR - FOR LOOPS FROM COLLIN
 
@@ -153,59 +149,59 @@
 //   console.log('hello world) *will log 5 times because i is 0 and the condition is 5
 //   console.log(rfpTeam[i]) // will log a different number as i; i = 0 then 1 then 2, etc..
 //   } */
-  
+
 // // //arrays and methods
 // // let team = ["Lucycan", "Collin", "Mark", "Cortez"];
-// // console.log(team); 
+// // console.log(team);
 // // console.log(team.length);
 
 // // ///push
 // // console.log(team.push("Derrick"));
-// // console.log(team); 
+// // console.log(team);
 
-// // ///pop 
+// // ///pop
 // // console.log(team.pop);
-// // console.log(team) 
+// // console.log(team)
 
-// // //join 
+// // //join
 // // console.log(team.join);
 
-// ///split returns a new array 
+// ///split returns a new array
 // // let team1 = "john,mike,kiersten,lucy";
 // // console.log(team1);
 // // let teamSplit = team1.split(",");
 // // console.log(teamSplit);
 
 // // ///Slice returns a new array
-// // console.log(team); 
-// // console.log(team.slice(0, 3)); 
-
-// // ///looping thru an array// 
 // // console.log(team);
-// // console.log(team[3]); 
-// // for (let i= 0; i < team.length; i++) 
+// // console.log(team.slice(0, 3));
+
+// // ///looping thru an array//
+// // console.log(team);
+// // console.log(team[3]);
+// // for (let i= 0; i < team.length; i++)
 // //   console.log(team[3]);
 
-// //   let roles = ["Teach Manager", 
-// //   "TechFello", 
-// //   "Placement Manager", 
-// //   "Program Manager", 
+// //   let roles = ["Teach Manager",
+// //   "TechFello",
+// //   "Placement Manager",
+// //   "Program Manager",
 // //   "Intern"
 // // ]
-// // ///Hi my name is Lucycan and I am the Tech Manager// 
+// // ///Hi my name is Lucycan and I am the Tech Manager//
 // // for (let i = 0; i < team.length; i++) {
 // // console.log(`Hello my name is ${team[i]} and I am a ${roles[i]}`);
 
 // // }
 
-// // ///Challenge// 
-// // let Team13 = ("Codiv8rs") 
+// // ///Challenge//
+// // let Team13 = ("Codiv8rs")
 // // let roles2 = ["Project Manager",
 // // "Technical Analyst",
 // // "Quality Assurance",
 // // ]
 
-// let team13 = prompt("What is your team Name?"); 
+// let team13 = prompt("What is your team Name?");
 // // let roles13 = prompt("Enter your team roles");
 // console.log(team13);
 
@@ -223,7 +219,7 @@
 
 // let arr = ['love', 'hat', 'die', 'good']
 // for (let initializer = 0; initializer < arr.length; ++initializer) {
-//   console.log (arr[initializer]) 
+//   console.log (arr[initializer])
 
 // }
 
@@ -232,21 +228,21 @@
 // console.log(arr[2])
 // for (let initializer = 0; initializer < arr.length; initializer++) {
 //  console.log(`hello world`)
-//  console.log(arr[initializer]) 
+//  console.log(arr[initializer])
 // }
 //ANCHOR - JAVASCRIPT FUNCTIONS
 
-// A reusable piece of code 
+// A reusable piece of code
 
 // function thisIsTheFunctionName(name) {
-// return name 
+// return name
 // }
 
 //TYPE THE FUNCTION NAME FOLLOWED BY PANRANTHESIS
 
 // thisIsTheFunctionName(`collin`) // the argument goes inside paranthesis
 
-// //RETURN STATEMENT 
+// //RETURN STATEMENT
 
 // let Name = thisIsTheFunctionName(`vince`)
 // let name2 = thisIsTheFunctionName( `collin`)
@@ -286,186 +282,307 @@
 // console.log(arr[initializer])
 // function21(array10)
 
-
 // }
 
 // loop()
 
- 
-
 // function blockScope(name){
-  
+
 //   let theKrew = ["Lucycan", "Collin", "Mark", "Cortez"];
-//   theKrew.push(name); 
+//   theKrew.push(name);
 //   console.log(theKrew);
 // }
-// let blanca = "Blanca"; 
+// let blanca = "Blanca";
 // blockScope(blanca);
 
 //// GAME JAVASCRIPT///
 
 // Background scrolling speed
 let move_speed = 3;
-    
+
 // Gravity constant value
-let gravity = 0.5;
-    
+let gravity = 0.05;
+
 // Getting reference to the bird element
-let bird = document.querySelector('.bird');
+let bird = document.querySelector(".bird");
 // Getting bird element properties
 let bird_props = bird.getBoundingClientRect();
-let background =
-    document.querySelector('.background')
-            .getBoundingClientRect();
-    
+let background = document.querySelector(".background").getBoundingClientRect();
+
 // Getting reference to the score element
-let score_val =
-    document.querySelector('.score_val');
-let message =
-    document.querySelector('.message');
-let score_title =
-    document.querySelector('.score_title');
-    
+let score_val = document.querySelector(".score_val");
+let message = document.querySelector(".message");
+let score_title = document.querySelector(".score_title");
+
 // Setting initial game state to start
-let game_state = 'Start';
-    
+let game_state = "Start";
+
+class FlappyBird {
+  bird = document.querySelector(".bird");
+  gameMessage = "";
+  gravity = 0.05;
+  background_Scrolling_Speed = 3;
+  score_val = document.querySelector(".score_val");
+  message = document.querySelector(".message");
+  score_title = document.querySelector(".score_title");
+  bird_props = bird.getBoundingClientRect();
+  bird_dy = 0;
+  currentlyJumping = false;
+  gameState = false;
+  pipe_sprite = document.querySelectorAll(".pipe_sprite");
+
+  constructor() {
+    if (!this.score_val && !this.message && !this.bird && !score_title) {
+      alert("Elements not found.");
+    }
+
+    window.addEventListener("keydown", (e) => {
+      // Start the game if enter key is pressed
+      if (e.key == "Enter" && !this.gameState) {
+        document.querySelectorAll(".pipe_sprite").forEach((e) => {
+          e.remove();
+        });
+        this.bird.style.top = "40vh";
+        this.gameState = true;
+        this.message.innerHTML = "";
+        this.score_title.innerHTML = "Score : ";
+        this.score_val.innerHTML = "0";
+        this.gameLoop();
+      }
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (!this.currentlyJumping && (e.key == "ArrowUp" || e.key == " ")) {
+        this.bird_dy = -3.4;
+        this.currentlyJumping = true;
+        setTimeout(() => {
+          this.currentlyJumping = false;
+        }, 50);
+      }
+    });
+
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    //movement code
+
+    if (3 % 1 == 1) {
+      if (!this.gameState) {
+        // Detect if game has ended
+        return;
+      }
+
+      this.pipe_sprite.forEach((element) => {
+        let pipe_sprite_props = element.getBoundingClientRect();
+        this.bird_props = bird.getBoundingClientRect();
+
+        // Delete the pipes if they have moved out
+        // of the screen hence saving memory
+        if (pipe_sprite_props.right <= 0) {
+          element.remove();
+        } else {
+          // Collision detection with bird and pipes
+          if (
+            this.bird_props.left <
+              this.pipe_sprite_props.left + pipe_sprite_props.width &&
+            this.bird_props.left + this.bird_props.width >
+              pipe_sprite_props.left &&
+            this.bird_props.top <
+              pipe_sprite_props.top + pipe_sprite_props.height &&
+            this.bird_props.top + this.bird_props.height > pipe_sprite_props.top
+          ) {
+            // Change game state and end the game
+            // if collision occurs
+            this.gameState = false;
+            this.message.innerHTML = "Press Enter To Restart";
+            this.message.style.left = "28vw";
+            return;
+          } else {
+            // Increase the score if player
+            // has the successfully dodged the
+            if (
+              pipe_sprite_props.right < this.bird_props.left &&
+              pipe_sprite_props.right + this.background_Scrolling_Speed >=
+                this.bird_props.left &&
+              element.increase_score == "1"
+            ) {
+              this.score_val.innerHTML =
+                Number.parseInt(this.score_val.innerHTML) + 1;
+            }
+            element.style.left =
+              pipe_sprite_props.left - this.background_Scrolling_Speed + "px";
+          }
+        }
+      });
+    }
+    this.bird_dy += this.gravity;
+    if (this.bird_props.top <= 0 || this.bird_props.bottom >= innerHeight) {
+      this.gameState = false;
+      this.message.innerHTML = "Press Enter To Restart";
+      this.message.style.left = "28vw";
+      return;
+    }
+    this.bird.style.top = this.bird_props.top + this.bird_dy + "px";
+    this.bird_props = this.bird.getBoundingClientRect();
+    //requestAnimationFrame(apply_gravity);
+
+    requestAnimationFrame(() => {
+      this.gameLoop();
+    });
+  }
+}
+let flappyBird = new FlappyBird();
+/* 
 // Add an eventlistener for key presses
-document.addEventListener('keydown', (e) => {
-    
+document.addEventListener("keydown", (e) => {
   // Start the game if enter key is pressed
-  if (e.key == 'Enter' &&
-      game_state != 'Play') {
-    document.querySelectorAll('.pipe_sprite')
-              .forEach((e) => {
+  if (e.key == "Enter" && game_state != "Play") {
+    document.querySelectorAll(".pipe_sprite").forEach((e) => {
       e.remove();
     });
-    bird.style.top = '40vh';
-    game_state = 'Play';
-    message.innerHTML = '';
-    score_title.innerHTML = 'Score : ';
-    score_val.innerHTML = '0';
+    bird.style.top = "40vh";
+    game_state = "Play";
+    message.innerHTML = "";
+    score_title.innerHTML = "Score : ";
+    score_val.innerHTML = "0";
     play();
   }
 });
+
 function play() {
+  let currentlyJumping = false;
   function move() {
-      
-    // Detect if game has ended
-    if (game_state != 'Play') return;
-      
-    // Getting reference to all the pipe elements
-    let pipe_sprite = document.querySelectorAll('.pipe_sprite');
-    pipe_sprite.forEach((element) => {
-        
-      let pipe_sprite_props = element.getBoundingClientRect();
-      bird_props = bird.getBoundingClientRect();
-        
-      // Delete the pipes if they have moved out
-      // of the screen hence saving memory
-      if (pipe_sprite_props.right <= 0) {
-        element.remove();
-      } else {
-        // Collision detection with bird and pipes
-        if (
-          bird_props.left < pipe_sprite_props.left +
-          pipe_sprite_props.width &&
-          bird_props.left +
-          bird_props.width > pipe_sprite_props.left &&
-          bird_props.top < pipe_sprite_props.top +
-          pipe_sprite_props.height &&
-          bird_props.top +
-          bird_props.height > pipe_sprite_props.top
-        ) {
-            
-          // Change game state and end the game
-          // if collision occurs
-          game_state = 'End';
-          message.innerHTML = 'Press Enter To Restart';
-          message.style.left = '28vw';
-          return;
-        } else {
-          // Increase the score if player
-          // has the successfully dodged the 
-          if (
-            pipe_sprite_props.right < bird_props.left &&
-            pipe_sprite_props.right + 
-            move_speed >= bird_props.left &&
-            element.increase_score == '1'
-          ) {
-            score_val.innerHTML = +score_val.innerHTML + 1;
-          }
-          element.style.left = 
-            pipe_sprite_props.left - move_speed + 'px';
-        }
+    if (3 % 1 == 1) {
+      if (game_state != "Play") {
+        // Detect if game has ended
+        return;
       }
-    });
+
+      // Getting reference to all the pipe elements
+      let pipe_sprite = document.querySelectorAll(".pipe_sprite");
+
+      pipe_sprite.forEach((element) => {
+        let pipe_sprite_props = element.getBoundingClientRect();
+        bird_props = bird.getBoundingClientRect();
+
+        // Delete the pipes if they have moved out
+        // of the screen hence saving memory
+        if (pipe_sprite_props.right <= 0) {
+          element.remove();
+        } else {
+          // Collision detection with bird and pipes
+          if (
+            bird_props.left <
+              pipe_sprite_props.left + pipe_sprite_props.width &&
+            bird_props.left + bird_props.width > pipe_sprite_props.left &&
+            bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height &&
+            bird_props.top + bird_props.height > pipe_sprite_props.top
+          ) {
+            // Change game state and end the game
+            // if collision occurs
+            game_state = "End";
+            message.innerHTML = "Press Enter To Restart";
+            message.style.left = "28vw";
+            return;
+          } else {
+            // Increase the score if player
+            // has the successfully dodged the
+            if (
+              pipe_sprite_props.right < bird_props.left &&
+              pipe_sprite_props.right + move_speed >= bird_props.left &&
+              element.increase_score == "1"
+            ) {
+              score_val.innerHTML = +score_val.innerHTML + 1;
+            }
+            element.style.left = pipe_sprite_props.left - move_speed + "px";
+          }
+        }
+      });
+    }
     requestAnimationFrame(move);
   }
-  requestAnimationFrame(move);
-  
+  move();
+
   let bird_dy = 0;
   function apply_gravity() {
-    console.log('hey')
-    if (game_state != 'Play') return;
-    bird_dy = bird_dy + gravity;
-    document.addEventListener('keydown', (e) => {
-      if (e.key == 'ArrowUp' || e.key == ' ') {
-        console.log('hey')
-        bird_dy = -7.6;
+    if (game_state != "Play") return;
+    bird_dy += gravity;
+
+    document.addEventListener("keydown", (e) => {
+      if (!currentlyJumping && (e.key == "ArrowUp" || e.key == " ")) {
+        bird_dy = -0.4;
+        currentlyJumping = true;
+        setTimeout(() => {
+          currentlyJumping = false;
+        }, 50);
       }
     });
-  
+
     // Collision detection with bird and
     // window top and bottom
-  
-    if (bird_props.top <= 0 ||
-        bird_props.bottom >= background.bottom) {
-      game_state = 'End';
-      message.innerHTML = 'Press Enter To Restart';
-      message.style.left = '28vw';
+
+    if (bird_props.top <= 0 || bird_props.bottom >= innerHeight) {
+      game_state = "End";
+      message.innerHTML = "Press Enter To Restart";
+      message.style.left = "28vw";
       return;
     }
-    bird.style.top = bird_props.top + bird_dy + 'px';
+    bird.style.top = bird_props.top + bird_dy + "px";
     bird_props = bird.getBoundingClientRect();
     requestAnimationFrame(apply_gravity);
   }
   requestAnimationFrame(apply_gravity);
-  
   let pipe_seperation = 0;
-    
+
   // Constant value for the gap between two pipes
   let pipe_gap = 35;
   function create_pipe() {
-    if (game_state != 'Play') return;
-      
+    if (game_state != "Play") return;
+
     // Create another set of pipes
     // if distance between two pipe has exceeded
     // a predefined value
     if (pipe_seperation > 115) {
-      pipe_seperation = 0
-        
+      pipe_seperation = 0;
+
       // Calculate random position of pipes on y axis
       let pipe_posi = Math.floor(Math.random() * 43) + 8;
-      let pipe_sprite_inv = document.createElement('div');
-      pipe_sprite_inv.className = 'pipe_sprite';
-      pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
-      pipe_sprite_inv.style.left = '100vw';
-        
+      let pipe_sprite_inv = document.createElement("div");
+      pipe_sprite_inv.className = "pipe_sprite";
+      pipe_sprite_inv.style.top = pipe_posi - 70 + "vh";
+      pipe_sprite_inv.style.left = "100vw";
+
       // Append the created pipe element in DOM
       document.body.appendChild(pipe_sprite_inv);
-      let pipe_sprite = document.createElement('div');
-      pipe_sprite.className = 'pipe_sprite';
-      pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
-      pipe_sprite.style.left = '100vw';
-      pipe_sprite.increase_score = '1';
-        
+      let pipe_sprite = document.createElement("div");
+      pipe_sprite.className = "pipe_sprite";
+      pipe_sprite.style.top = pipe_posi + pipe_gap + "vh";
+      pipe_sprite.style.left = "100vw";
+      pipe_sprite.increase_score = "1";
+
       // Append the created pipe element in DOM
       document.body.appendChild(pipe_sprite);
     }
     pipe_seperation++;
     requestAnimationFrame(create_pipe);
   }
+  document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter" && game_state != "Play") {
+      document.querySelectorAll(".pipe_sprite").forEach((e) => {
+        e.remove();
+      });
+      bird.style.top = "40vh";
+      game_state = "Play";
+      message.innerHTML = "";
+      score_title.innerHTML = "Score : ";
+      score_val.innerHTML = "0";
+      play();
+    }
+  });
   requestAnimationFrame(create_pipe);
+  requestAnimationFrame(play);
 }
 
-play()
+play();
+ */
